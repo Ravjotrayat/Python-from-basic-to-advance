41e19d61631b7b79caf599bce05efc5cdc36b2e8
Ravjot Singh Rayat <94758593+Ravjotrayat@users.noreply.github.com>
2023-04-02 01:24:04 +0530

    Update Flight_ticket.py

--- DAY 12/Flight_ticket.py
+++ DAY 12/Flight_ticket.py
@@ -1,3 +1,19 @@
+'''
+Write a python program to help an airport manager to generate
+ few statistics based on the ticket details available for a 
+day.
+Go through the below program and complete it based on the comments mentioned in it.
+Note: Perform case sensitive string comparisons wherever necessary.
+#Sample ticket list - ticket format: "flight_no:source:destination:ticket_no"
+#Note: flight_no has the following format - "airline_name followed by three digit number
+
+#Global variable
+ticket_list=["AI567:MUM:LON:014","AI077:MUM:LON:056","BA896:MUM:LON:067","SI267:MUM:SIN:145","AI077:MUM:CAN:060","SI267:BLR:MUM:148","AI567:CHE:SIN:015","AI077:MUM:SIN:050","AI077:MUM:LON:051","SI267:MUM:SIN:146"]
+print(find_passengers_flight("AI"))
+print(find_passengers_destination("LON"))
+find_passengers_per_flight()
+print(sort_passenger_list())'''
+
 ticket_list=["AI567:MUM:LON:014","AI077:MUM:LON:056","BA896:MUM:LON:067",
              "SI267:MUM:SIN:145","AI077:MUM:CAN:060","SI267:BLR:MUM:148",
              "AI567:CHE:SIN:015","AI077:MUM:SIN:050","AI077:MUM:LON:051",
